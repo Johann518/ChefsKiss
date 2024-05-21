ede9f92481d0451720b3f8a3e35cd5248c206dec
Johann518 <165226536+Johann518@users.noreply.github.com>
2024-05-21 14:17:40 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,39 @@
+import Pages from "./pages/Pages";
+import Category from "./components/Category";
+import { BrowserRouter } from "react-router-dom";
+import Search from "./components/Search";
+import styled from "styled-components";
+import { LuChefHat } from "react-icons/lu";
+import { Link } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <Nav>
+          <LuChefHat />
+          <Logo to={'/'}>Chefs Kiss</Logo>
+      </Nav>
+        <Search />
+        <Category />
+        <Pages />
+      </BrowserRouter>
+    </div>
+  );
+}
+const Logo = styled(Link)`
+  text-decoration: none;
+  font-size: 1.5rem;
+  font-weight: 400;
+  font:family: 'Lobster Two', cursive;
+`
+const Nav = styled.div`
+  padding: 4rem 0rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  svg {
+    font-size: 2rem;
+  }
+`
+export default App;
